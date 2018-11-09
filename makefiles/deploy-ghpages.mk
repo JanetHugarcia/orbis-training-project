f632387fa9ab14927adf7d148b6246ff1454ff6b
Marlon Inga C <marlon.inga@metricaandina.com>
2018-11-09 14:45:27 -0500

    add jenkinsfile

--- makefiles/deploy-ghpages.mk
+++ makefiles/deploy-ghpages.mk
@@ -3,11 +3,11 @@
 DEPLOY_DIR = app/deploy
 BUILD_DIR = build
 GIT_BRANCH = gh-pages
-GIT_PERSONAL_TOKEN = 3b644847665d1ec116a2f151141bbbb3b85df84d
+GIT_PERSONAL_TOKEN = e035756fe6be07a8593756baafa3115af5850caf
 GIT_BRANCH_DIR = $(PWD)/$(DEPLOY_DIR)/$(GIT_BRANCH)
 
 define mkdir_deploy_dir
-	@if [ ! -d "$(GIT_BRANCH_DIR)" ]; then mkdir $(GIT_BRANCH_DIR); fi
+    @if [ ! -d "$(GIT_BRANCH_DIR)" ]; then mkdir $(GIT_BRANCH_DIR); fi
 endef
 
 define git_init
@@ -32,52 +32,52 @@ define git_add_remote_repository
 endef
 
 define create_branch_gh_pages
-	@cd $(GIT_BRANCH_DIR) && \
-	 git checkout -b $(GIT_BRANCH)
+    @cd $(GIT_BRANCH_DIR) && \
+     git checkout -b $(GIT_BRANCH)
 endef
 
 define copy_files_to_deploy
-	@cp -r $(PWD)/$(DEPLOY_DIR)/$(BUILD_DIR)/* $(GIT_BRANCH_DIR)
+    @cp -r $(PWD)/$(DEPLOY_DIR)/$(BUILD_DIR)/* $(GIT_BRANCH_DIR)
 endef
 
 define git_add
-	@cd $(GIT_BRANCH_DIR) && \
-	 git add * && \
-	 git status
+    @cd $(GIT_BRANCH_DIR) && \
+     git add * && \
+     git status
 endef
 
 define create_commit
-	$(eval MESSAGE := $(shell git log --pretty=format:"%s" | head -n 1))
-	@cd $(GIT_BRANCH_DIR) && \
-	 git commit -m "$(MESSAGE)"
+    $(eval MESSAGE := $(shell git log --pretty=format:"%s" | head -n 1))
+    @cd $(GIT_BRANCH_DIR) && \
+     git commit -m "$(MESSAGE)"
 endef
 
 define git_push
-	@cd $(GIT_BRANCH_DIR) && \
-	 git push origin $(GIT_BRANCH) --force
+    @cd $(GIT_BRANCH_DIR) && \
+     git push origin $(GIT_BRANCH) --force
 endef
 
 define clean_workspace
-	@rm -rf $(GIT_BRANCH_DIR)
+    @rm -rf $(GIT_BRANCH_DIR)
 endef
 
 define show_deploy_url
-	$(eval GIT_REPOSITORY_REMOTE := $(shell git remote -v | grep origin | grep '(push)'| awk '{print $$2}'))
-	$(eval GIT_REPOSITORY_REMOTE_SSH := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | grep 'git@'))
+    $(eval GIT_REPOSITORY_REMOTE := $(shell git remote -v | grep origin | grep '(push)'| awk '{print $$2}'))
+    $(eval GIT_REPOSITORY_REMOTE_SSH := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | grep 'git@'))
 
-	$(ifeq ($(strip $(GIT_REPOSITORY_REMOTE_SSH)),), \
-		$(eval GIT_USER_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 1 | cut -d ":" -f 2)), \
-		$(eval GIT_USER_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 4)) \
-	)
+    $(ifeq ($(strip $(GIT_REPOSITORY_REMOTE_SSH)),), \
+        $(eval GIT_USER_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 1 | cut -d ":" -f 2)), \
+        $(eval GIT_USER_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 4)) \
+    )
 
-	$(ifeq ($(strip $(GIT_REPOSITORY_REMOTE_SSH)),), \
-		$(eval GIT_REPOSITORY_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 2)), \
-		$(eval GIT_REPOSITORY_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 5 | sed "s/.git//g" | sed "s/(push)//g")) \
-	)
+    $(ifeq ($(strip $(GIT_REPOSITORY_REMOTE_SSH)),), \
+        $(eval GIT_REPOSITORY_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 2)), \
+        $(eval GIT_REPOSITORY_NAME := $(shell echo '$(GIT_REPOSITORY_REMOTE)' | cut -d "/" -f 5 | sed "s/.git//g" | sed "s/(push)//g")) \
+    )
 
-	@echo ""
-	@echo "Publicado en: http://$(GIT_USER_NAME).github.io/$(GIT_REPOSITORY_NAME)"
-	@echo ""
+    @echo ""
+    @echo "Publicado en: http://$(GIT_USER_NAME).github.io/$(GIT_REPOSITORY_NAME)"
+    @echo ""
 endef
 
 deploy.ghpages:
@@ -91,3 +91,4 @@ deploy.ghpages:
 	$(call create_commit)
 	$(call git_push)
 	$(call clean_workspace)
+	$(call show_deploy_url)	
